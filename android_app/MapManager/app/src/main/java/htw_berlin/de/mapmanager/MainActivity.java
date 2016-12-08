4cc0da9310e9afb065f962ec62c8b311cc444322
tognimat <matteo.togni93@gmail.com>
2016-12-08 13:34:31 +0100

    Removed useless improt

--- android_app/MapManager/app/src/main/java/htw_berlin/de/mapmanager/MainActivity.java
+++ android_app/MapManager/app/src/main/java/htw_berlin/de/mapmanager/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
