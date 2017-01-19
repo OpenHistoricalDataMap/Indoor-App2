cecd9eeaca8f0747d7e8167d89e8a6481335b240
tognimat <matteo.togni93@gmail.com>
2017-01-19 21:41:32 +0100

    Added settings by Carola. Modified therefore AndroidManifest (info about Activity), modified MainActivity to add menu item call to the SettingsActivity, prefs and prefs.ui packages. string_arrays.xml

---
+++ android_app/MapManager/app/src/main/java/htw_berlin/de/mapmanager/prefs/SettingsActivity.java
@@ -0,0 +1,16 @@
+package htw_berlin.de.mapmanager.prefs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Display the fragment as the main content.
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
+}
